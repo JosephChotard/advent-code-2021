8aead522f2bec98128e7b8f57a05b29c7a07edc6
Joseph Chotard <josephchotard1@gmail.com>
2022-09-27 18:15:03 -0400

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     a();
+    b();
 }
 
 fn a() {
@@ -8,7 +9,7 @@ fn a() {
         .fold([0, 0], |[h, d], line| {
             let direction = &line[..line.len() - 2];
             let distance = line[line.len() - 1..]
-                .parse::<i32>()
+                .parse::<u32>()
                 .expect("This should be a number");
             match direction {
                 "forward" => [h + distance, d],
@@ -17,5 +18,23 @@ fn a() {
                 _ => panic!("Not a valid direction"),
             }
         });
-    println!("Final answer {:?}", new_pos[0] * new_pos[1]);
+    println!("Final answer of day 2 part 1 {:?}", new_pos[0] * new_pos[1]);
+}
+
+fn b() {
+    let new_pos = include_str!("../inputb.txt")
+        .lines()
+        .fold([0, 0, 0], |[h, d, a], line| {
+            let direction = &line[..line.len() - 2];
+            let distance = line[line.len() - 1..]
+                .parse::<u32>()
+                .expect("This should be a number");
+            match direction {
+                "forward" => [h + distance, d + a * distance, a],
+                "down" => [h, d, a + distance],
+                "up" => [h, d, a - distance],
+                _ => panic!("Not a valid direction"),
+            }
+        });
+    println!("Final answer of day 2 part 2 {:?}", new_pos[0] * new_pos[1]);
 }
