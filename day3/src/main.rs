a78731c784640512c27f634b18893e7a5d3b514b
Joseph Chotard <josephchotard1@gmail.com>
2022-09-29 00:09:13 -0400

    feat: day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -35,4 +35,73 @@ fn a() {
     println!("Answer to day 3 part 1 is {:?}", gamma_rate * epsilon_rate);
 }
 
-fn b() {}
+const INPUT_SIZE: usize = 12;
+fn b() {
+    let inputb: Vec<[u32; INPUT_SIZE]> = include_str!("../inputb.txt")
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(2).expect("Input contains a non digit"))
+                .collect::<Vec<u32>>()
+                .try_into()
+                .expect("input wrong size")
+        })
+        .collect();
+
+    let ox_gen_rating = {
+        let mut indexes: Vec<usize> = (0..inputb.len()).collect();
+        let mut bit_pos = 0;
+        while indexes.len() > 1 {
+            // Get most common value at bit_pos
+            let num_of_one = indexes
+                .iter()
+                .filter(|index| inputb[**index][bit_pos] == 1)
+                .count();
+            let value_to_filter = if num_of_one as f32 >= (indexes.len() as f32 / 2.0) {
+                1
+            } else {
+                0
+            };
+            indexes = indexes
+                .into_iter()
+                .filter(|i| inputb[*i][bit_pos] == value_to_filter)
+                .collect();
+            bit_pos += 1;
+        }
+        // Convert last input to number
+        inputb[indexes[0]].iter().fold(0, |a, b| 2 * a + b)
+    };
+
+    let co2_scrub_rating = {
+        let mut indexes: Vec<usize> = (0..inputb.len()).collect();
+        let mut bit_pos = 0;
+        while indexes.len() > 1 {
+            // Get most common value at bit_pos
+            let num_of_one = indexes
+                .iter()
+                .filter(|index| inputb[**index][bit_pos] == 1)
+                .count();
+            let value_to_filter = if num_of_one as f32 >= (indexes.len() as f32 / 2.0) {
+                0
+            } else {
+                1
+            };
+            indexes = indexes
+                .into_iter()
+                .filter(|i| inputb[*i][bit_pos] == value_to_filter)
+                .collect();
+            bit_pos += 1;
+        }
+        // Convert last input to number
+        inputb[indexes[0]].iter().fold(0, |a, b| 2 * a + b)
+    };
+
+    println!(
+        "ox rating is {:?}, co2 srub rating is {:?}",
+        ox_gen_rating, co2_scrub_rating
+    );
+    println!(
+        "Life support rating is {:?}",
+        ox_gen_rating * co2_scrub_rating
+    );
+}
