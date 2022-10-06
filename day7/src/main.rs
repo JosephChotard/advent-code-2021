088cb6b8773525d1e91dfce3569e6319aa5c2456
Joseph Chotard <josephchotard1@gmail.com>
2022-10-06 13:35:46 +0100

    d7 p2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     a();
+    b();
 }
 
 fn a() {
@@ -22,4 +23,24 @@ fn a() {
     println!("Total fuel for crabs 1 is {}", total_fuel);
 }
 
-fn b() {}
+fn b() {
+    let nums = include_str!("../input.txt")
+        .split(",")
+        .map(|n| n.parse::<usize>().expect("input is not num"))
+        .collect::<Vec<usize>>();
+    let average: usize = nums.iter().sum::<usize>() / nums.len();
+    // Allows us to check the average rounded up and down and choose the minimum of both
+    let total_fuel: usize = (average..)
+        .take(2)
+        .map(|a| {
+            nums.iter()
+                .map(|num| {
+                    let distance = a.abs_diff(*num);
+                    (distance * (distance + 1)) / 2
+                })
+                .sum()
+        })
+        .min()
+        .expect("No min found");
+    println!("Total fuel for crabs 2 is {}", total_fuel);
+}
