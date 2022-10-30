4573c452f4d6417d4c98c21bb3e5e3be099f6b7d
Joseph Chotard <josephchotard1@gmail.com>
2022-10-30 23:26:25 +0000

    d9 p2

--- day9/main.py
+++ day9/main.py
@@ -1,20 +1,47 @@
-def part1():
-    grid = []
-    with open('input.txt') as f:
-        for line in f:
-            grid.append([int(c) for c in list(line.strip())])
-    
-    lowest_sum = 0
+import heapq
+from functools import reduce
+
+grid = []
+with open('input.txt') as f:
+    for line in f:
+        grid.append([int(c) for c in list(line.strip())])
+
+def get_neighbour_pos(x,y):
+    for (i,j) in [[x-1,y], [x+1,y], [x,y-1], [x,y+1]]:
+        if i >= 0 and i < len(grid) and j >= 0 and j < len(grid[0]):
+            yield (i,j)
+
+def get_low_point_coord():
+    low_points = []
     for x, row in enumerate(grid):
         for y, value in enumerate(row):
             lowest = True
-            for (i,j) in [[x-1,y], [x+1,y], [x,y-1], [x,y+1]]:
-                if i >= 0 and i < len(grid) and j >= 0 and j < len(row):
-                    if grid[i][j] <= value:
-                        lowest = False
+            for (i,j) in get_neighbour_pos(x,y):
+                if grid[i][j] <= value:
+                    lowest = False
             if lowest:
-                lowest_sum += value+1
-    return lowest_sum
+                low_points.append((x,y))
+    return low_points
+
+
+def part1():
+    return reduce(lambda accum, coord: grid[coord[0]][coord[1]]+1+accum, get_low_point_coord(), 0)
+
+def size_of_basin(x,y, visited=set()) -> int:
+    size = 0
+    if (x,y) not in visited and grid[x][y] < 9:
+        size = 1
+        visited.add((x,y))
+        for (i,j) in get_neighbour_pos(x,y):
+            size += size_of_basin(i,j,visited)
+    return size
+
+
+def part2() -> int:
+    three_largest= heapq.nlargest(3, [size_of_basin(x,y) for (x,y) in get_low_point_coord()])
+    return reduce((lambda x,y: x*y), three_largest)
 
 if __name__ == '__main__':
     print(part1())
+    print(size_of_basin(0,0))
+    print(part2())
