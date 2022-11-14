a3a1a6da98cee53525e0ad6012137fc4a921491c
Joseph Chotard <jchotard@palantir.com>
2022-11-14 16:03:24 +0000

    d11 p2

--- day11/main.py
+++ day11/main.py
@@ -37,5 +37,33 @@ def a():
     
     print(f"A. Flashes: {flashes}")
 
+def b():
+    grid = parse_grid()
+    step = 0
+    all_flashed = False
+    while not all_flashed:
+        step += 1
+        for y in range(len(grid)):
+            for x in range(len(grid)):
+                grid[y][x] += 1
+        is_flashable = True
+        flash_count = 0
+        while is_flashable:
+            is_flashable = False
+            for y in range(len(grid)):
+                for x in range(len(grid)):
+                    if grid[y][x] > 9:
+                        is_flashable = True
+                        grid[y][x] = 0
+                        flash_count += 1
+                        for (n_x, n_y) in get_neighbour_pos(x,y,grid):
+                            if grid[n_y][n_x] != 0:
+                                grid[n_y][n_x] += 1
+        if flash_count == len(grid)*len(grid):
+            all_flashed = True
+    
+    print(f"B. Step: {step}")
+
 if __name__ == '__main__':
     a()
+    b()
