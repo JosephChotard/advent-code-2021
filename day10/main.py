c73726b1415ef289ac0286c60ab74c6f491bb6aa
Joseph Chotard <jchotard@palantir.com>
2022-11-14 11:11:26 +0000

    d10 p2

--- day10/main.py
+++ day10/main.py
@@ -16,14 +16,15 @@ opposite = {
     "{": "}"
 }
 
-points = {
-    "]": 57,
-    ")": 3,
-    "}": 1197,
-    ">": 25137
-}
+
 
 def count_mismatched(line):
+    points = {
+        "]": 57,
+        ")": 3,
+        "}": 1197,
+        ">": 25137
+    }
     stack = deque()
     for char in line[0]:
         if char not in "[]()<>{}":
@@ -37,12 +38,44 @@ def count_mismatched(line):
                 return points[char]
     return 0
 
+def score_incomplete_lines(line):
+    points = {
+        "]": 2,
+        ")": 1,
+        "}": 3,
+        ">": 4
+    }
+    stack = deque()
+    for char in line[0]:
+        if char not in "[]()<>{}":
+            raise ValueError(f"character {char} not in set of valid chars")
+        if char in opposite.keys():
+            stack.append(char)
+        else:
+            if opposite[stack[-1]] == char:
+                stack.pop()
+            else:
+                return 0
+    # Calculate score from left over stack chars
+    score = 0
+    while stack:
+        score *= 5
+        score += points[opposite[stack.pop()]]
+    return score
+
+
 def a():
     df1 = spark.read.text("input.txt")
     score = df1.rdd.map(count_mismatched)
-    print(score.sum())
+    print(f"a: {score.sum()}")
 
-
+def b():
+    df1 = spark.read.text("input.txt")
+    scores = df1.rdd.map(score_incomplete_lines).filter(lambda x:x!=0).collect()
+    scores.sort()
+    median = scores[len(scores)//2]
+    print(f"b: {median}")
 
 if __name__ == '__main__':
     a()
+    b()
